6b6933426fb72b15f9c594496850c873bd8d49c4
Lukas Werner <lukas@wernerresearch.com>
2020-07-06 15:34:44 -0700

    Updates

---
+++ precache-manifest.4cf421c78ac29e8430e32000abcfa28f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3824876673243c9608c80cff98865f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b25d23222498360f28e",
+    "url": "/static/css/main.88175eff.chunk.css"
+  },
+  {
+    "revision": "0d7cde33d7de4b9f2616",
+    "url": "/static/js/2.1fea8a3f.chunk.js"
+  },
+  {
+    "revision": "a2fd88e3257da34c29c48adcd9ceb0a6",
+    "url": "/static/js/2.1fea8a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b25d23222498360f28e",
+    "url": "/static/js/main.022073c0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "80afbb9ace994f5af5ac393ed1eb836c",
+    "url": "/static/media/logo.80afbb9a.svg"
+  }
+]);
